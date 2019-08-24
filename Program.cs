b0122adc8aad34b89301c77cbd0346c0dec5e165
Rafael Almeida da Silva <rafael.silva_Pessoal@outlook.com>
2019-08-24 11:01:19 -0300

    Add example of project's observer
    Project's design patterns observer example.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace exSurpresaAniversarioObserver
+{
+    public class ChegadaAniversarianteEvent
+    {   
+        public DateTime horaDeChegada{get; set;}
+    }
+    interface IChegadaAniversariante
+    {
+        void chegou(ChegadaAniversarianteEvent event);
+    }
+
+// abstract class BaseThread
+// {
+//   private Thread _thread;
+
+//   protected BaseThread() { _thread = new Thread(new ThreadStart(this.RunThread)); }
+
+//   // Thread methods / properties
+//   public void Start() { _thread.Start(); }
+//   public void Join() { _thread.Join(); }
+//   public bool IsAlive { get { return _thread.IsAlive; } }
+
+//   // Override in base class
+//   public abstract void RunThread();
+// }
+
+// public MyThread : BaseThread
+// {
+//   public override void RunThread() {
+//     // Do some stuff
+//   }
+// }
+    class Porteiro
+    {
+
+    }
+
+    class Namorada: IChegadaAniversariante
+    {
+        public void chegou(ChegadaAniversarianteEvent event){}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program.java
@@ -0,0 +1,10 @@
+import java.util;
+
+class Program 
+{
+    static void main(string[] args)
+    {
+        System.out.println("hello world");
+    }
+
+}
